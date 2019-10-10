0fef53484b8601364821006b82ad47bf861901f3
Jason Grace <jason@thegracefamily.net>
2019-10-09 23:27:12 -0400

    Problem 2 - CS

---
+++ problems/2/cs/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int fib_1 = 1;
+            int fib_2 = 2;
+
+            while (fib_2 < 4_000_000)
+            {
+                if (fib_2 % 2 ==0)
+                {
+                    sum += fib_2;
+                }
+                int temp = fib_2;
+                fib_2 = fib_1 + fib_2;
+                fib_1 = temp;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
