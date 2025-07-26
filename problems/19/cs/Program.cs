552f95d061ceacf1efdffb9b8eb63d96d43d202f
Jason Grace <jason@thegracefamily.net>
2025-07-26 13:23:58 -0400

    Problem 19

---
+++ problems/19/cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sundays = 0;
+
+            DateTime date1 = new DateTime(1901, 1, 1);
+            DateTime date2 = new DateTime(2000, 12, 31);
+
+            for (DateTime date = date1; date <= date2; date = date.AddMonths(1)) {
+                if (date.DayOfWeek == DayOfWeek.Sunday) {
+                    sundays += 1;
+                }
+            }
+
+            Console.WriteLine(sundays);
+        }
+    }
+}
