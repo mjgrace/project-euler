d2cc62ee78e04f27dfbc3d1679e8f1f510a33890
Jason Grace <jason@thegracefamily.net>
2025-07-26 14:53:48 -0400

    Problem 5

---
+++ problems/5/cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            bool matchFound = false;
+
+            for (num = 1; !matchFound; num++) {
+                for (int j = 1; num % j == 0 && j <= 20; j++){
+                    if (j == 20) {
+                        matchFound = true;
+                        Console.WriteLine(num);
+                    }
+                }
+            }
+        }
+    }
+}
